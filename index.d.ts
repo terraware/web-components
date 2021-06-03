00ceb0f6f2aec22aa80916ad5ebf3a4b2f28e779
constanzauanini <constanza.uanini@admios-sa.com>
2021-06-03 10:00:15 -0300

    Change export

--- index.d.ts
+++ index.d.ts
@@ -1,170 +1,26 @@
-import { KeyboardEventHandler } from "react";
+import { Props as AutocompleteProps } from "./src/components/Autocomplete";
+import { Props as CancelButtonProps } from "./src/components/CancelButton";
+import { Props as CheckboxProps } from "./src/components/Checkbox";
+import { Props as DatePickerProps } from "./src/components/DatePicker";
+import { Props as DialogCloseButtonProps } from "./src/components/DialogCloseButton";
+import { Props as DivisorProps } from "./src/components/Divisor";
+import { Props as DropdownProps } from "./src/components/Dropdown";
+import { Props as NoteProps } from "./src/components/Note";
+import { Props as RadioButtonProps } from "./src/components/RadioButton";
+import { Props as SummaryBoxProps } from "./src/components/SummaryBox";
+import { Props as TableProps } from "./src/components/table";
+import { Props as TextFieldProps } from "./src/components/TextField";
 
-declare module "web-components";
-
-export interface Autocomplete {
-  id: string;
-  label: string;
-  values: string[];
-  onChange: (id: string, value: string) => void;
-  selected: string | undefined;
-}
-
-export interface CancelButton {
-  onClick: () => void;
-  label?: string;
-}
-
-export interface Checkbox {
-  id: string;
-  name: string;
-  label: React.ReactNode;
-  value?: boolean | null;
-  onChange: (id: string, value: boolean) => void;
-  disabled?: boolean;
-}
-
-export interface DatePicker {
-  id: string;
-  label: React.ReactNode;
-  value?: string | null;
-  onChange: (id: string, value?: string) => void;
-  "aria-label": string;
-  onKeyPress?: KeyboardEventHandler;
-  maxDate?: string;
-  error?: boolean;
-  helperText?: string;
-  disabled?: boolean;
-}
-
-export interface DialogCloseButton {
-  onClick: () => void;
-}
-
-export interface Divisor {
-  mt?: number;
-}
-
-export type DropdownItem = {
-  label: string;
-  value: string;
-};
-
-export interface Dropdown {
-  id: string;
-  label: string;
-  values?: DropdownItem[];
-  onChange: (id: string, value: string) => void;
-  selected: string | undefined;
-  disabled?: boolean;
-}
-
-export interface Note {
-  children: string;
-}
-
-export interface RadioButton {
-  id: string;
-  name: string;
-  label: React.ReactNode;
-  value?: boolean | null;
-  onChange: (id: string, value: boolean) => void;
-}
-
-export interface SummaryBox {
-  id?: string;
-  title: string;
-  value: number | string;
-  variant?: "default" | "available" | "zero" | "full";
-  icon?: boolean;
-  onIconClick?: () => void;
-}
-
-export interface TextArea {
-  id: string;
-  value: unknown | null;
-  onChange: (id: string, value: unknown) => void;
-  type?: React.InputHTMLAttributes<unknown>["type"];
-  label: React.ReactNode;
-  disabled?: boolean;
-  endAdornment?: JSX.Element;
-  onKeyPress?: KeyboardEventHandler;
-  min?: number;
-  error?: boolean;
-  helperText?: string;
-  placeholder?: string;
-  required?: boolean;
-}
-
-export interface TextField {
-  id: string;
-  value: unknown | null;
-  onChange: (id: string, value: unknown) => void;
-  type?: React.InputHTMLAttributes<unknown>["type"];
-  label: React.ReactNode;
-  disabled?: boolean;
-  endAdornment?: JSX.Element;
-  onKeyPress?: KeyboardEventHandler;
-  min?: number;
-  error?: boolean;
-  helperText?: string;
-  placeholder?: string;
-  required?: boolean;
-}
-
-export interface Table<T> {
-  id?: string;
-  orderBy: string;
-  order?: Order;
-  columns: TableColumnType[];
-  rows: T[];
-  Renderer?: (props: RendererProps<T>) => JSX.Element;
-  onSelect?: (value: T) => void;
-  DetailsRenderer?: (props: DetailsRendererProps) => JSX.Element;
-  sortComparator?: (a: T, b: T, orderBy: keyof T) => 1 | -1 | 0;
-  sortHandler?: (order: Order, orderBy: string) => void;
-  isInactive?: (row: T) => boolean;
-  onReorderEnd?: ({ oldIndex, newIndex }: any) => void;
-  isClickable?: (row: T) => boolean;
-}
-
-export type Order = "asc" | "desc";
-
-export interface TableColumnType {
-  key: string;
-  name: string;
-  type: "string" | "number" | "date" | "notes" | "edit" | "boolean";
-}
-
-export interface RendererProps<T> {
-  index: number;
-  row: T;
-  column: TableColumnType;
-  value?: string | number | unknown[];
-  onRowClick?: () => void;
-}
-
-export type EnhancedTableDetailsRow = {
-  [x: string]: string | number | [] | undefined;
-};
-
-export interface DetailsProps<T> {
-  accessionId: string;
-  index: number;
-  expandText: string;
-  rowName: string;
-  defaultSort: string;
-  columns: TableColumnType[];
-  onClick: (parentValue: EnhancedTableDetailsRow) => void;
-  onSelect: (
-    value: EnhancedTableDetailsRow,
-    parentValue: EnhancedTableDetailsRow
-  ) => void;
-  Renderer: (props: RendererProps<T>) => JSX.Element;
-  row: EnhancedTableDetailsRow;
-}
-
-export interface DetailsRendererProps {
-  index: number;
-  row: EnhancedTableDetailsRow;
-}
+export function Autocomplete(props: AutocompleteProps): JSX.Element;
+export function CancelButton(props: CancelButtonProps): JSX.Element;
+export function Checkbox(props: CheckboxProps): JSX.Element;
+export function DatePicker(props: DatePickerProps): JSX.Element;
+export function DialaogCloseButton(props: DialogCloseButtonProps): JSX.Element;
+export function Divisor(props: DivisorProps): JSX.Element;
+export function Dropdown(props: DropdownProps): JSX.Element;
+export function Note(props: NoteProps): JSX.Element;
+export function RadioButton(props: RadioButtonProps): JSX.Element;
+export function SummaryBox(props: SummaryBoxProps): JSX.Element;
+export function TextArea(props: TextFieldProps): JSX.Element;
+export function TextField(props: TextFieldProps): JSX.Element;
+export function Table<T>(props: TableProps<T>): JSX.Element;
