19ddc6703c07cda859f7a8c2c2fdf7947165ba9b
constanzauanini <constanza.uanini@admios-sa.com>
2021-06-28 16:20:40 -0300

    Adding destructive and disabled option. Also adding focus

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,25 +1,29 @@
-import React from "react";
-import "./styles.scss";
+import React from 'react';
+import './styles.scss';
 
 export interface Props {
   onClick: () => void;
   label: string;
-  type: "productive" | "passive";
-  priority: "primary" | "secondary";
-  size: "small" | "medium" | "large" | "xlarge";
+  type: 'productive' | 'passive' | 'destructive';
+  priority: 'primary' | 'secondary';
+  size: 'small' | 'medium' | 'large' | 'xlarge';
+  disabled: boolean;
 }
 
-export default function Button({
-  onClick,
-  label,
-  type = "productive",
-  priority = "primary",
-  size = "small",
-}: Props): JSX.Element {
+export default function Button(props: Props): JSX.Element {
+  const {
+    onClick,
+    label,
+    type = 'productive',
+    priority = 'primary',
+    size = 'small',
+    disabled = false,
+  } = props;
   return (
     <button
       onClick={onClick}
-      className={`button ${type}-${priority} button--${size}`}
+      className={`button ${type}-${priority} button--${size} ${type}-${priority}--${size}`}
+      disabled={disabled}
     >
       {label}
     </button>
