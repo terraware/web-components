57ff7011fd929c3e1cb3cd964420162dadc2bb25
constanzauanini <constanza.uanini@admios-sa.com>
2021-06-29 10:48:52 -0300

    Remove undefined options from story

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -7,6 +7,20 @@ import Button, { Props as ButtonProps } from '../components/Button/Button';
 export default {
   title: 'Button',
   component: Button,
+  argTypes: {
+    type: {
+      options: ['productive', 'passive', 'destructive'],
+      control: { type: 'radio' },
+    },
+    priority: {
+      options: ['primary', 'secondary'],
+      control: { type: 'radio' },
+    },
+    size: {
+      options: ['small', 'medium', 'large', 'xlarge'],
+      control: { type: 'radio' },
+    },
+  },
 };
 
 const Template: Story<ButtonProps> = (args) => {
