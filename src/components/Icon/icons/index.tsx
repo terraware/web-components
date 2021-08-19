44da8b4d9a08d0735d6709f79bb06a04310f8ca1
constanzauanini <constanza.uanini@admios-sa.com>
2021-08-19 13:33:06 -0300

    Modify button (#6)

--- src/components/Icon/icons/index.tsx
+++ src/components/Icon/icons/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { ReactComponent as CaretDown } from './caret-down.svg';
 import { ReactComponent as Lock } from './lock.svg';
+import { ReactComponent as Plus } from './plus.svg';
 import { ReactComponent as Spinner } from './spinner.svg';
 
-export type IconName = 'lock' | 'spinner';
+export type IconName = 'lock' | 'spinner' | 'caretDown' | 'plus';
 
 type SVGComponent = React.FunctionComponent<
   React.SVGProps<SVGSVGElement> & {
@@ -12,6 +14,8 @@ type SVGComponent = React.FunctionComponent<
 const icons: Record<IconName, SVGComponent> = {
   lock: Lock,
   spinner: Spinner,
+  caretDown: CaretDown,
+  plus: Plus,
 };
 
 export default icons;
