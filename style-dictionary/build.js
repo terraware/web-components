abcd40fe430297913def27070d440161327697fa
constanzauanini <constanza.uanini@admios-sa.com>
2021-08-20 11:14:15 -0300

    New tokens

--- style-dictionary/build.js
+++ style-dictionary/build.js
@@ -1,25 +1,28 @@
 function build(source, destination) {
   const StyleDictionary = require('style-dictionary').extend({
-    "source": source,
-    "platforms": {
-      "scss": {
-        "transformGroup": "scss",
-        "transforms": ["name/css_colors", "name/css_size", "name/css_typography"],
-        "buildPath": "../style-dictionary-dist/",
-        "files": [{
-          "format": "scss/variables",
-          "destination": destination,
-        }]
-      }
-    }
+    source: source,
+    platforms: {
+      scss: {
+        transformGroup: 'scss',
+        transforms: ['name/css_colors', 'name/css_size', 'name/css_composite', 'name/css_typography'],
+        buildPath: '../style-dictionary-dist/',
+        files: [
+          {
+            format: 'scss/variables',
+            destination: destination,
+          },
+        ],
+      },
+    },
   });
-  
+
   StyleDictionary.registerTransform(require('./transforms/name_css_colors.js'));
+  StyleDictionary.registerTransform(require('./transforms/name_css_composite.js'));
   StyleDictionary.registerTransform(require('./transforms/name_css_typography.js'));
   StyleDictionary.registerTransform(require('./transforms/name_css_size.js'));
-  
+
   StyleDictionary.buildAllPlatforms();
 }
 
-build(["./json/button/**/*.json"], "button_variables.scss");
-build(["./json/system/**/*.json"], "system_variables.scss");
+build(['./json/button/**/*.json'], 'button_variables.scss');
+build(['./json/system/**/*.json'], 'system_variables.scss');
