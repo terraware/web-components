881a19d4d35a7627b814e52ae4873cdd0e384c0b
constanzauanini <constanza.uanini@admios-sa.com>
2021-08-27 13:36:32 -0300

    Feature/nav (#8)
    * Nabbar components
    
    * Navbar styles
    
    * Expand label
    
    * Adding on click
    
    * Adding logo and fix some stiles
    
    * Interactions
    
    * Fix error
    
    * Add some styles
    
    * Fix linter
    
    * Fix linter
    
    * Fix tests

---
+++ src/stories/Navbar.stories.tsx
@@ -0,0 +1,47 @@
+// YourComponent.stories.js
+
+import { Story } from '@storybook/react';
+import React from 'react';
+import Navbar, { Props as NavbarProps } from '../components/Navbar/Navbar';
+import NavItem from '../components/Navbar/NavItem';
+import NavSection from '../components/Navbar/NavSection';
+import SubNavbar from '../components/Navbar/SubNavbar';
+
+export default {
+  title: 'Navbar',
+  component: Navbar,
+  argTypes: {},
+};
+
+const Template: Story<NavbarProps> = (args) => {
+  const [selectedItem, setSelectedItem] = React.useState('projects');
+
+  const onClickHandler = (open: boolean | undefined) => {
+    if (open) {
+      setSelectedItem('summary');
+    } else {
+      setSelectedItem('home');
+    }
+  };
+
+  return (
+    <Navbar>
+      <NavItem label='Home' icon='home' selected={selectedItem === 'home'} onClick={() => setSelectedItem('home')} />
+      <NavItem label='Seeds' icon='seeds' onClick={onClickHandler}>
+        <SubNavbar>
+          <NavItem label='Summary' selected={selectedItem === 'summary'} onClick={() => setSelectedItem('summary')} />
+          <NavItem label='Accessions' selected={selectedItem === 'accessions'} onClick={() => setSelectedItem('accessions')} />
+        </SubNavbar>
+      </NavItem>
+      <NavItem label='Plants' icon='restorationSite' selected={selectedItem === 'plants'} onClick={() => setSelectedItem('plants')} />
+      <NavItem label='Species' icon='species' selected={selectedItem === 'species'} onClick={() => setSelectedItem('species')} />
+      <NavSection />
+      <NavItem label='Projects' icon='folder' selected={selectedItem === 'projects'} onClick={() => setSelectedItem('projects')} />
+      <NavItem label='Sites' icon='site' selected={selectedItem === 'sites'} onClick={() => setSelectedItem('sites')} />
+      <NavSection />
+      <NavItem label='Admin' icon='key' selected={selectedItem === 'admin'} onClick={() => setSelectedItem('admin')} />
+    </Navbar>
+  );
+};
+
+export const Default = Template.bind({});
--- style-dictionary/generate.sh
+++ style-dictionary/generate.sh
@@ -18,9 +18,16 @@ curl https://terraformation.zeroheight.com/api/token_file/c51547b17edb/share > j
 curl https://terraformation.zeroheight.com/api/token_file/27fbda37a0d5/share > json/system/size-and-space.json
 curl https://terraformation.zeroheight.com/api/token_file/e1207808261b/share > json/system/color-iconography.json
 curl https://terraformation.zeroheight.com/api/token_file/2adb63d624f6/share > json/system/size-iconography.json
+curl https://terraformation.zeroheight.com/api/token_file/381a96fe2ed1/share > json/system/navbar-text-composite.json
+curl https://terraformation.zeroheight.com/api/token_file/193dc5a8d136/share > json/system/color-navbar.json
+curl https://terraformation.zeroheight.com/api/token_file/decd4c60f4f9/share > json/system/size-navbar.json
+curl https://terraformation.zeroheight.com/api/token_file/c4a77df9398c/share > json/system/color-divider.json
+curl https://terraformation.zeroheight.com/api/token_file/f789865ce7b7/share > json/system/size-divider.json
+sleep 10
 curl https://terraformation.zeroheight.com/api/token_file/cb5cd890ddf4/share > json/button/typography.json
 curl https://terraformation.zeroheight.com/api/token_file/2f90d49d4334/share > json/button/productive-colors.json
 curl https://terraformation.zeroheight.com/api/token_file/5e072293caf0/share > json/button/passive-colors.json
 curl https://terraformation.zeroheight.com/api/token_file/a170e0886291/share > json/button/destructive-colors.json
 curl https://terraformation.zeroheight.com/api/token_file/cfb0cfaf26bf/share > json/button/size.json
+
 node build.js
