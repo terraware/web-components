f319c11bc8c48cfe979c634b441579b0d7623c53
constanzauanini <constanza.uanini@admios-sa.com>
2021-08-27 13:30:10 -0300

    Fix tests

--- src/components/Navbar/SubNavbar.tsx
+++ src/components/Navbar/SubNavbar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './styles.scss';
 
-export interface Props {
+export interface SubNavbarProps {
   children: JSX.Element | JSX.Element[];
 }
 
-export default function SubNavbar(props: Props): JSX.Element {
+export default function SubNavbar(props: SubNavbarProps): JSX.Element {
   const { children } = props;
 
   return <div>{children}</div>;
