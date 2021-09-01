4b39918809e5416fb847c715c5fc5e378cdf35ae
constanzauanini <constanza.uanini@admios-sa.com>
2021-09-01 09:43:59 -0300

    Using linear-gradient

--- style-dictionary/build.js
+++ style-dictionary/build.js
@@ -4,7 +4,7 @@ function build(source, destination) {
     platforms: {
       scss: {
         transformGroup: 'scss',
-        transforms: ['name/css_colors', 'name/css_size', 'name/css_composite', 'name/css_typography', 'value/gradient'],
+        transforms: ['name/css_composite', 'value/gradient'],
         buildPath: '../style-dictionary-dist/',
         files: [
           {
@@ -16,10 +16,7 @@ function build(source, destination) {
     },
   });
 
-  StyleDictionary.registerTransform(require('./transforms/name_css_colors.js'));
   StyleDictionary.registerTransform(require('./transforms/name_css_composite.js'));
-  StyleDictionary.registerTransform(require('./transforms/name_css_typography.js'));
-  StyleDictionary.registerTransform(require('./transforms/name_css_size.js'));
   StyleDictionary.registerTransform(require('./transforms/value_gradient.js'));
 
   StyleDictionary.buildAllPlatforms();
--- style-dictionary/transforms/value_gradient.js
+++ style-dictionary/transforms/value_gradient.js
@@ -6,14 +6,11 @@ module.exports = {
     return prop.filePath.endsWith('gradient.json');
   },
   transformer: function (prop) {
-    //console.log(prop);
     let value = prop.value;
     if (prop.original && prop.original.value.startsWith('linear-gradient')) {
-      console.log('entra');
       const expression = `{${value.path}}`;
       var re = new RegExp(expression, 'g');
       value = prop.original.value.replace(re, prop.value.value);
-      console.log(value);
     }
     return value;
   },
