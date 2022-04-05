0de4c9df4d45b79e6d474ed41a703d2340b4d4dc
constanzauanini <constanza.uanini@admios-sa.com>
2022-04-05 16:32:14 -0300

    TRACK-407 Add progress circle component

---
+++ src/stories/ProgressCircle.stories.tsx
@@ -0,0 +1,14 @@
+// YourComponent.stories.js
+
+import { Story } from '@storybook/react';
+import React from 'react';
+import ProgressCircle, { Props as ProgressCircleProps } from '../components/ProgressCircle/ProgressCircle';
+
+export default {
+  title: 'Progress Circle',
+  component: ProgressCircle,
+};
+
+const Template: Story<ProgressCircleProps> = (args) => <ProgressCircle {...args} />;
+
+export const Default = Template.bind({});
