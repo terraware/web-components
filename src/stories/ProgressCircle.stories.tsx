4a5a4d503e0e6da3f8779d3a297c278d7b6b6c60
constanzauanini <constanza.uanini@admios-sa.com>
2022-04-06 10:07:00 -0300

    Update ProgressCircle.stories.tsx

--- src/stories/ProgressCircle.stories.tsx
+++ src/stories/ProgressCircle.stories.tsx
@@ -1,5 +1,3 @@
-// YourComponent.stories.js
-
 import { Story } from '@storybook/react';
 import React from 'react';
 import ProgressCircle, { Props as ProgressCircleProps } from '../components/ProgressCircle/ProgressCircle';
