7cc028fdb45549e294b2b1ef9e22f10d4cb22607
constanzauanini <constanza.uanini@admios-sa.com>
2022-05-11 17:35:20 -0300

    Add different configurations stories

--- src/stories/DialogBox.stories.tsx
+++ src/stories/DialogBox.stories.tsx
@@ -14,16 +14,65 @@ const Template: Story<DialogBoxProps> = (args) => {
   return <DialogBox {...args} />;
 };
 
-export const Default = Template.bind({});
+export const Small = Template.bind({});
+Small.args = {
+  title: 'Title',
+  message: 'Message',
+  size: 'small',
+  middleButtons: [
+    <Button id='new-species' label='Test' onClick={() => true} size='small' />,
+    <Button id='new-species' label='Test' onClick={() => true} size='small' />,
+  ],
+};
 
-Default.args = {
+export const Medium = Template.bind({});
+Medium.args = {
+  title: 'Title',
+  message: 'Message',
+  size: 'medium',
+  middleButtons: [
+    <Button id='new-species' label='Test' onClick={() => true} size='small' />,
+    <Button id='new-species' label='Test' onClick={() => true} size='small' />,
+  ],
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  title: 'Title',
+  message: 'Message',
+  size: 'large',
+  middleButtons: [
+    <Button id='new-species' label='Test' onClick={() => true} size='small' />,
+    <Button id='new-species' label='Test' onClick={() => true} size='small' />,
+  ],
+};
+
+export const XLarge = Template.bind({});
+XLarge.args = {
+  title: 'Title',
+  message: 'Message',
+  size: 'x-large',
+  middleButtons: [
+    <Button id='new-species' label='Test' onClick={() => true} size='small' />,
+    <Button id='new-species' label='Test' onClick={() => true} size='small' />,
+  ],
+};
+
+export const threeButtons = Template.bind({});
+threeButtons.args = {
+  title: 'Title',
+  message: 'Message',
+  size: 'medium',
   leftButton: <Button id='new-species' label='Test' onClick={() => true} size='small' />,
   rightButtons: [
     <Button id='new-species' label='Test' onClick={() => true} size='small' key='1' />,
     <Button id='new-species' label='Test' onClick={() => true} size='small' key='2' />,
   ],
-  // middleButtons: [
-  //   <Button id='new-species' label='Test' onClick={() => true} size='small' />,
-  //   <Button id='new-species' label='Test' onClick={() => true} size='small' />,
-  // ],
+};
+
+export const noFooter = Template.bind({});
+noFooter.args = {
+  title: 'Title',
+  message: 'Message',
+  size: 'small',
 };
