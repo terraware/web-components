9ab98e3cf64abad2d77bfdb2b1456d7c17f454c0
Josh Buchea <verctrl@joshbuchea.com>
2022-07-08 12:17:40 -1000

    add react native transform

--- style-dictionary/build.js
+++ style-dictionary/build.js
@@ -1,11 +1,33 @@
-function build(source, destination) {
+function buildMobile(source, destination) {
+  const StyleDictionary = require('style-dictionary').extend({
+    source: source,
+    platforms: {
+      rn: {
+        transformGroup: 'react-native',
+        // transforms: [],
+        // TODO: determine if any transforms are needed
+        buildPath: '../style-dictionary-dist/mobile/',
+        files: [
+          {
+            destination: destination,
+            format: 'javascript/es6',
+          },
+        ],
+      },
+    },
+  });
+
+  StyleDictionary.buildAllPlatforms();
+}
+
+function buildWeb(source, destination) {
   const StyleDictionary = require('style-dictionary').extend({
     source: source,
     platforms: {
       scss: {
         transformGroup: 'scss',
         transforms: ['name/css_composite', 'value/gradient', 'value/shadow'],
-        buildPath: '../style-dictionary-dist/',
+        buildPath: '../style-dictionary-dist/web/',
         files: [
           {
             format: 'scss/variables',
@@ -23,8 +45,12 @@ function build(source, destination) {
   StyleDictionary.buildAllPlatforms();
 }
 
-build(['./json/system/**/*.json', './json/button/**/*.json'], 'button_variables.scss');
-build(['./json/system/**/*.json', './json/navbar/**/*.json'], 'navbar_variables.scss');
-build(['./json/system/**/*.json', './json/textfield/**/*.json'], 'textfield_variables.scss');
-build(['./json/system/**/*.json', './json/progressCircle/**/*.json'], 'progressCircle_variables.scss');
-build(['./json/system/**/*.json', './json/dialogBox/**/*.json'], 'dialogBox_variables.scss');
+// build mobile
+buildMobile(['./json/**/*.json'], 'style-variables.js');
+
+// build web
+buildWeb(['./json/system/**/*.json', './json/button/**/*.json'], 'button_variables.scss');
+buildWeb(['./json/system/**/*.json', './json/navbar/**/*.json'], 'navbar_variables.scss');
+buildWeb(['./json/system/**/*.json', './json/textfield/**/*.json'], 'textfield_variables.scss');
+buildWeb(['./json/system/**/*.json', './json/progressCircle/**/*.json'], 'progressCircle_variables.scss');
+buildWeb(['./json/system/**/*.json', './json/dialogBox/**/*.json'], 'dialogBox_variables.scss');
