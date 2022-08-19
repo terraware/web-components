0d4a4ee56034fa6aa461e590753600d88a4c5b08
Karthik Balachandran <karthik@terraformation.com>
2022-08-19 13:37:01 -0700

    test npmrc

--- .github/scripts/update-npm.sh
+++ .github/scripts/update-npm.sh
@@ -7,17 +7,18 @@ echo "Artifact Registry Provider = $ARTIFACT_REGISTRY_PROVIDER"
 # TODO: add support for AWS Code artifact
 if [ $ARTIFACT_REGISTRY_PROVIDER == 'npm' ]; then
   cp .npmrc.github .npmrc
+  cat .npmrc | sed "s/npm_.*/npm_xxxxx/"
   echo "Checking npm registry for current version"
   NPM_PACKAGE_VERSION=`npm show @terraware/web-components version`
   echo "Got npm package version $NPM_PACKAGE_VERSION"
   echo "Checking version in git package.json"
-  PACKAGE_JSON_VERSION=`node -e "console.log(require(__dirname + '../../dist/package.json').version)"`
+  PACKAGE_JSON_VERSION=`node -e "process.stdout.write(require(__dirname + '../../dist/package.json').version)"`
   echo "Got package json version $PACKAGE_JSON_VERSION"
   if [ $PACKAGE_JSON_VERSION == $NPM_PACKAGE_VERSION ]; then
     echo "Package is up to date"
   else
     echo "Updating package in npm"
-    cd dist && npm publish
+    cd dist && npm publish --userconfig=../.npmrc
     echo "Done updating package in npm"
   fi
 else
