a217dcc332511a2478ec5c1a14fe80b9baec66fb
Karthik Balachandran <karthik@terraformation.com>
2022-08-19 12:04:01 -0700

    test scripts

---
+++ .github/scripts/set-environment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Define tier based on branch ref
+# For production, we will tag the commit similar to how we release BE/FE
+if [[ "$GITHUB_REF" =~ refs/tags/(v[0-9]+\.[0-9.]+) ]]; then
+  ARTIFACT_REGISTRY=registry.npmjs.org
+  ARTIFACT_REGISTRY_PROVIDER=npm
+  ARTIFACT_REGISTRY_AUTH_TOKEN=$NPM_REGISTRY_AUTH_TOKEN
+elif [[ "$GITHUB_REF" == refs/heads/main ]]; then
+  # TODO: use AWS code artifact repository for staging
+  # For now, staging takes care of prod npm update.
+  ARTIFACT_REGISTRY=registry.npmjs.org
+  ARTIFACT_REGISTRY_PROVIDER=npm
+  ARTIFACT_REGISTRY_AUTH_TOKEN=$NPM_REGISTRY_AUTH_TOKEN
+else
+  echo "Nothing to do"
+#  exit
+  ARTIFACT_REGISTRY=registry.npmjs.org
+  ARTIFACT_REGISTRY_PROVIDER=npm
+  ARTIFACT_REGISTRY_AUTH_TOKEN=$NPM_REGISTRY_AUTH_TOKEN
+fi
+
+# Set registry envars
+echo "ARTIFACT_REGISTRY=$ARTIFACT_REGISTRY
+ARTIFACT_REGISTRY_PROVIDER=$ARTIFACT_REGISTRY_PROVIDER
+ARTIFACT_REGISTRY_AUTH_TOKEN=$ARTIFACT_REGISTRY_AUTH_TOKEN" >> $GITHUB_ENV
---
+++ .github/scripts/update-npm.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Artifact Registry Provider = $ARTIFACT_REGISTRY_PROVIDER"
+
+# TODO: add support for AWS Code artifact
+if [ $ARTIFACT_REGISTRY_PROVIDER == 'npm' ]; then
+  cp .npmrc.github .npmrc
+  echo "Checking npm registry for current version"
+  NPM_PACKAGE_VERSION=`npm show @terraware/web-components version`
+  echo "Got npm package version $NPM_PACKAGE_VERSION"
+  echo "Checking version in git package.json"
+  PACKAGE_JSON_VERSION=`node -e "console.log(require(__dirname + '../../dist/package.json').version)"`
+  echo "Got package json version $PACKAGE_JSON_VERSION"
+  if [ $PACKAGE_JSON_VERSION == $NPM_PACKAGE_VERSION ]; then
+    echo "Package is up to date"
+  else
+    echo "Updating package in npm"
+    cd dist && npm publish
+    echo "Done updating package in npm"
+  fi
+else
+  echo "Nothing to do"
+  exit
+fi
