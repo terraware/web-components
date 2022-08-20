5a30e9df96ee1b4f29899809f1e9d4451e64e1c2
Karthik Balachandran <karthik@terraformation.com>
2022-08-19 17:16:27 -0700

    SW-1446 Update github workflow to publish npm package - publish package to npm in staging and production if there's a version change

---
+++ .github/scripts/compare-artifact-package-versions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Artifact Registry Provider = $ARTIFACT_REGISTRY_PROVIDER"
+
+# TODO: add support for AWS Code artifact
+if [ $ARTIFACT_REGISTRY_PROVIDER == 'npm' ]; then
+
+  echo "Checking npm registry for current version"
+  NPM_PACKAGE_VERSION=`npm show @terraware/web-components version`
+  echo "Got npm package version $NPM_PACKAGE_VERSION"
+
+  echo "Checking version in git package.json"
+  PACKAGE_JSON_VERSION=`node -e "process.stdout.write(require(__dirname + '../../dist/package.json').version)"`
+  echo "Got package json version $PACKAGE_JSON_VERSION"
+
+  if [ $PACKAGE_JSON_VERSION == $NPM_PACKAGE_VERSION ]; then
+    echo "Package is up to date"
+  else
+    echo "Package needs update"
+    echo "UPDATE_NPM_PACKAGE=true" >> $GITHUB_ENV
+  fi
+
+else
+
+  echo "Nothing to do"
+  exit
+
+fi
---
+++ .github/scripts/set-environment.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Define tier based on branch ref
+# For production, we will tag the commit similar to how we release BE/FE
+if [[ "$GITHUB_REF" =~ refs/tags/(v[0-9]+\.[0-9.]+) ]]; then
+  ARTIFACT_REGISTRY_PROVIDER=npm
+elif [[ "$GITHUB_REF" == refs/heads/main ]]; then
+  # TODO: use AWS code artifact repository for staging
+  # For now, staging takes care of prod npm update.
+  ARTIFACT_REGISTRY_PROVIDER=npm
+else
+  echo "Nothing to do"
+fi
+
+# Set registry envars
+echo "ARTIFACT_REGISTRY_PROVIDER=$ARTIFACT_REGISTRY_PROVIDER" >> $GITHUB_ENV
