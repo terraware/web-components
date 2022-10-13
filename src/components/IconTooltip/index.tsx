9efd91cb79141ce4ae8c399561e053e9a2940673
Josh Buchea <joshbuchea@users.noreply.github.com>
2022-10-13 13:05:53 -1000

    SW-1760: Dashboard Total Active Accessions Tooltip (#82)
    SW-1760: Dashboard Total Active Accessions Tooltip (#82)

--- src/components/IconTooltip/index.tsx
+++ src/components/IconTooltip/index.tsx
@@ -36,8 +36,17 @@ export type IconTooltipProps = {
 };
 
 export default function IconTooltip({ iconName = 'info', placement = 'top-start', title }: IconTooltipProps): JSX.Element {
+  const [open, setOpen] = React.useState(false);
   const { isMobile } = useDeviceInfo();
   const classes = useStyles();
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
 
   return (
     <Tooltip
@@ -46,13 +55,15 @@ export default function IconTooltip({ iconName = 'info', placement = 'top-start'
         arrow: classes.arrow,
         tooltip: classes.tooltip,
       }}
+      onClose={handleTooltipClose}
+      open={isMobile ? open : undefined}
       placement={placement}
       sx={{
         maxWidth: isMobile ? '342px' : '464px',
       }}
       title={title}
     >
-      <span>
+      <span onClick={handleTooltipOpen}>
         <Icon name={iconName} className={classes.icon} />
       </span>
     </Tooltip>
