7fe0a42276ee06791b3061f3de9953a79fc07600
Charlie Keith <114949086+charlie-tf@users.noreply.github.com>
2022-10-18 11:08:57 -0600

    SW-2016 Fix Numerical Sorting (#88)
    SW-2016 Fix Numerical Sorting
    
    - Default comparator first attempts parseFloat on both values
    - If successful, then compare the resulting numeric values
    - If either is NaN, then fall back to default (string) compare
    - Add some unit tests for the sort

--- src/components/table/sort.ts
+++ src/components/table/sort.ts
@@ -1,6 +1,14 @@
-export function descendingComparator<T>(a: T, b: T, orderBy: keyof T): 1 | -1 | 0 {
+export function descendingComparator<T>(a: T, b: T, orderBy: keyof T): number {
+  // first attempt to parse into a numeric value and compare
+  const numCompare = descendingNumComparator(a, b, orderBy);
+  if (numCompare !== null) {
+    return numCompare;
+  }
+
+  // if non-numeric, compare using the javascript built-in compare for this type
   const bValue = b[orderBy] ?? '';
   const aValue = a[orderBy] ?? '';
+
   if (bValue < aValue) {
     return -1;
   }
@@ -11,12 +19,22 @@ export function descendingComparator<T>(a: T, b: T, orderBy: keyof T): 1 | -1 | 
   return 0;
 }
 
+function descendingNumComparator<T>(a: T, b: T, orderBy: keyof T): number | null {
+  const aNumValue = parseFloat((a[orderBy] ?? '0.0') as string);
+  const bNumValue = parseFloat((b[orderBy] ?? '0.0') as string);
+  if (!isNaN(aNumValue) && !isNaN(bNumValue)) {
+    return bNumValue - aNumValue;
+  }
+
+  return null;
+}
+
 export type SortOrder = 'asc' | 'desc';
 
 export function getComparator<Key extends keyof any>(
   order: SortOrder,
   orderBy: Key,
-  sorting: (a: any, b: any, orderBy: any) => 1 | -1 | 0
+  sorting: (a: any, b: any, orderBy: any) => number
 ): (a: { [key in Key]?: string | number | [] }, b: { [key in Key]?: string | number | [] }) => number {
   return order === 'desc' ? (a, b) => sorting(a, b, orderBy) : (a, b) => -sorting(a, b, orderBy);
 }
