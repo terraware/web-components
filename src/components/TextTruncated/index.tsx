4c45f4dd585f4b48e28c59b1582a4a42a8219f00
Charlie Keith <charlie@terraformation.com>
2022-10-25 16:00:15 -0600

    SW-1860 Truncate and Show More List of Strings
    - create a component that shows a list of string
    - given a max length, truncate the list
    - display a link button indicating the remaining count
    - clicking the link button shows a tooltip with the full list

---
+++ src/components/TextTruncated/index.tsx
@@ -0,0 +1,127 @@
+import { Link, Theme, Tooltip, Typography, TypographyProps } from '@mui/material';
+import { useState } from 'react';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  arrow: {
+    color: `${theme.palette.TwClrBg} !important`,
+    '&:before': {
+      border: `1px solid ${theme.palette.TwClrBrdrTertiary}`
+    },
+  },
+  tooltip: {
+    backgroundColor: `${theme.palette.TwClrBg} !important`,
+    color: `${theme.palette.TwClrTxt} !important`,
+    border: `1px solid ${theme.palette.TwClrBrdrTertiary} !important`,
+    borderRadius: '8px !important',
+    boxShadow: `0px 3px 3px ${theme.palette.TwClrBaseGray100}`,
+    padding: '8px',
+  },
+}));
+
+export interface Props {
+  stringList: string[];
+  maxChars: number;
+  moreSeparator?: string;
+  moreText?: string;
+  textStyle?: TypographyProps['sx'];
+  showAllStyle?: TypographyProps['sx'];
+}
+
+/**
+ * Represents the result of truncating the string list
+ * text: the text to display before the '...# more' link
+ * numberMore: the number to display in the '...# more' link
+ *   has two special values
+ *   0: the full text can be displayed; do not show '...# more'
+ *   -1: the list is of length one but the element cannot be fully
+ *       displayed. show '...more' without a '#'
+ */
+interface TextDisplay {
+  text: string;
+  numberMore: number;
+}
+
+/**
+ * Compute TextDisplay for a list of strings
+ * @param strings list of strings to process
+ * @param maxLength max displayable length
+ */
+function computeFromStringList(
+  strings: string[],
+  maxLength: number,
+): TextDisplay {
+  if (strings[0].length > maxLength) {
+    // case in which the first string is too long to fit by itself
+    return {
+      text: strings[0].substring(0, maxLength),
+      numberMore: strings.length === 1 ? -1 : strings.length - 1
+    };
+  }
+
+  // concatenate strings with ', ' and stop before exceeding max
+  let stringsRemaining = strings.length - 1;
+  let result = strings[0];
+  for (let i = 1; i < strings.length; i++) {
+    const tempResult = [result, strings[i]].join(', ');
+    if (tempResult.length > maxLength) {
+      break;
+    }
+    stringsRemaining--;
+    result = tempResult;
+  }
+
+  return {
+    text: result,
+    numberMore: stringsRemaining,
+  };
+}
+
+export default function TextTruncated(props: Props): JSX.Element {
+  const {
+    stringList,
+    maxChars,
+    moreSeparator = '... ',
+    moreText = 'more',
+    textStyle,
+    showAllStyle,
+  } = props;
+  const [showAllOpen, setShowAllOpen] = useState(false);
+  const classes = useStyles();
+  const maxExcludingSuffix =
+    maxChars - moreSeparator.length - moreText.length - 1 - Math.ceil(Math.log10(stringList.length));
+  const textToDisplay = computeFromStringList(stringList, maxExcludingSuffix);
+
+  return (
+    <Typography sx={textStyle}>
+      {textToDisplay.text}{textToDisplay.numberMore !== 0 ? moreSeparator : ''}
+      {textToDisplay.numberMore !== 0
+        ? (<Tooltip
+            arrow={true}
+            classes={{
+              arrow: classes.arrow,
+              tooltip: classes.tooltip,
+            }}
+            open={showAllOpen}
+            title={(
+              <Typography sx={showAllStyle}>
+                {stringList.join(', ')}
+              </Typography>
+            )}
+          >
+            <Link
+              component='button'
+              onClick={() => setShowAllOpen(!showAllOpen)}
+              onBlur={() => setShowAllOpen(false)}
+            >
+              <Typography sx={{...textStyle, marginTop: '-3px'}}>{
+                (textToDisplay.numberMore > 0 ? textToDisplay.numberMore + ' ' : '') + moreText
+              }</Typography>
+            </Link>
+          </Tooltip>
+        )
+        : null
+      }
+    </Typography>
+  );
+}
