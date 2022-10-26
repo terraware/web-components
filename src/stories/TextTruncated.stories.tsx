ce84629cccc78d624578230865ee5b1f681f3979
Charlie Keith <114949086+charlie-tf@users.noreply.github.com>
2022-10-26 14:36:37 -0600

    SW-1860 Truncate and Show More List of Strings (#107)
    - create a component that shows a list of strings
    - given a max length in pixels, compute the max characters
    that are allowed for display
    - truncate the list to the max character length
    - display a link button indicating the remaining count
    - clicking the link button shows a tooltip with the full list

---
+++ src/stories/TextTruncated.stories.tsx
@@ -0,0 +1,23 @@
+import { Story } from '@storybook/react';
+import React from 'react';
+import TextTruncated, {
+  Props as TextTruncatedProps,
+} from '../components/TextTruncated/index';
+
+export default {
+  title: 'TextTruncated',
+  component: TextTruncated,
+};
+
+const Template: Story<TextTruncatedProps> = (args) => {
+  return <TextTruncated {...args} />;
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  stringList: ['elephant', 'foo', 'bar', 'baz', 'spam', 'bacon', 'eggs'],
+  maxLengthPx: 200,
+  showAllStyle: {fontSize: 14},
+  textStyle: {fontSize: 14},
+};
