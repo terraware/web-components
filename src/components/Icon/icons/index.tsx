2ff73298777fc609a3ba1c05adfa69014b2e0cce
Karthik B <karthik@terraformation.com>
2022-11-18 07:06:13 -0800

    Add photo/image asset to visual-updates branch (#153)
    - mostly cherry picked from main

--- src/components/Icon/icons/index.tsx
+++ src/components/Icon/icons/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import BlobbyGrayIconImage from '../../svg/BlobbyGrayIconImage';
 import BlobbyGrayIconUploadToTheCloud from '../../svg/BlobbyGrayIconUploadToTheCloud';
 import BlobbyIconHappy from '../../svg/BlobbyIconHappy';
 import BlobbyIconLeaf from '../../svg/BlobbyIconLeaf';
@@ -76,6 +77,7 @@ import Warning from '../../svg/Warning';
 import Wifi from '../../svg/IconWifi';
 
 export type IconName =
+  | 'blobbyGrayIconImage'
   | 'blobbyGrayIconUploadToTheCloud'
   | 'blobbyIconSeedBank'
   | 'blobbyIconHappy'
@@ -156,6 +158,7 @@ type SVGComponent = React.FunctionComponent<
   }
 >;
 const icons: Record<IconName, SVGComponent> = {
+  blobbyGrayIconImage: BlobbyGrayIconImage,
   blobbyGrayIconUploadToTheCloud: BlobbyGrayIconUploadToTheCloud,
   blobbyIconSeedBank: BlobbyIconSeedBank,
   blobbyIconHappy: BlobbyIconHappy,
