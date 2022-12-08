a6af051e91acd850516c851e30e12d57a47ccc78
Karthik Balachandran <karthik@terraformation.com>
2022-12-08 08:55:42 -0800

    SW-2490 Checkbox style fixes
    - border radius and width are not fixed since it's an icon not css
    - added comment in figma for ^
    - more fixes to be done in web repo

--- src/stories/Checkbox.stories.tsx
+++ src/stories/Checkbox.stories.tsx
@@ -1,12 +1,19 @@
 import { action } from '@storybook/addon-actions';
 import { Story } from '@storybook/react';
 import React from 'react';
+import { makeStyles } from '@mui/styles';
 import Checkbox, { Props as CheckboxProps } from '../components/Checkbox';
 
 export default {
   title: 'Checkbox',
   component: Checkbox,
 };
+
+const useStyles = makeStyles(() => ({
+  narrowWidth: {
+    width: '200px',
+  },
+}));
 
 const Template: Story<CheckboxProps> = (args) => {
   const [value, setValue] = React.useState(true);
@@ -15,14 +22,26 @@ const Template: Story<CheckboxProps> = (args) => {
     setValue(_value);
   };
 
-  return <Checkbox {...args} onChange={toggleChange} value={value} />;
+  const classes = useStyles();
+  const className = classes[args.useClassName];
+
+  return <Checkbox className={className} {...args} onChange={toggleChange} value={value} />;
 };
 
 export const Default = Template.bind({});
+export const TopAlignLongLabel = Template.bind({});
 
 Default.args = {
   id: '1',
   name: 'Test',
-  label: <p>Test</p>,
+  label: 'Test',
   disabled: false,
+};
+
+TopAlignLongLabel.args = {
+  id: '2',
+  name: 'Alignment Test',
+  label: 'The Quick Brown Fox Jumped Over The Silly Lazy Goat',
+  disabled: false,
+  useClassName: 'narrowWidth',
 };
