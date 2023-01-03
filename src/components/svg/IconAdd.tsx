9d9c668cbc8410f7bf1804b8f9bbac6898740424
Karthik Balachandran <karthik@terraformation.com>
2023-01-03 08:46:34 -0800

    This difference is from running `yarn generate-assets` which is affected by the dependency version change. The diffs to all the files is removal of a line between interface and class. Not sure why, seems like a bad change but...

--- src/components/svg/IconAdd.tsx
+++ src/components/svg/IconAdd.tsx
@@ -4,12 +4,10 @@ interface SVGRProps {
   title?: string;
   titleId?: string;
 }
-
 const SvgIconAdd = ({ title, titleId, ...props }: SVGProps<SVGSVGElement> & SVGRProps) => (
   <svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' aria-labelledby={titleId} {...props}>
     {title ? <title id={titleId}>{title}</title> : null}
     <path d='M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2Zm0 1.5c4.703 0 8.5 3.797 8.5 8.5 0 4.703-3.797 8.5-8.5 8.5A8.489 8.489 0 0 1 3.5 12c0-4.703 3.797-8.5 8.5-8.5Zm-.012 3.49a.75.75 0 0 0-.738.76v3.5h-3.5a.751.751 0 1 0 0 1.5h3.5v3.5a.751.751 0 1 0 1.5 0v-3.5h3.5a.751.751 0 1 0 0-1.5h-3.5v-3.5a.75.75 0 0 0-.762-.76Z' />
   </svg>
 );
-
 export default SvgIconAdd;
--- src/components/svg/IconFile.tsx
+++ src/components/svg/IconFile.tsx
@@ -4,12 +4,10 @@ interface SVGRProps {
   title?: string;
   titleId?: string;
 }
-
 const SvgIconFile = ({ title, titleId, ...props }: SVGProps<SVGSVGElement> & SVGRProps) => (
   <svg viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' aria-labelledby={titleId} {...props}>
     {title ? <title id={titleId}>{title}</title> : null}
     <path d='M6.25 2A2.261 2.261 0 0 0 4 4.25v15.5A2.261 2.261 0 0 0 6.25 22h11.5A2.261 2.261 0 0 0 20 19.75V9.25a.75.75 0 0 0-.22-.53l-.008-.008L13.28 2.22a.75.75 0 0 0-.53-.22h-6.5Zm0 1.5H12v4.25A2.261 2.261 0 0 0 14.25 10h4.25v9.75c0 .423-.327.75-.75.75H6.25a.739.739 0 0 1-.75-.75V4.25c0-.423.327-.75.75-.75Zm7.25 1.06 3.94 3.94h-3.19a.739.739 0 0 1-.75-.75V4.56Z' />
   </svg>
 );
-
 export default SvgIconFile;
