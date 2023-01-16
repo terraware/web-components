f8c898229a2f54e3fcbee415fe7084b2b71ef6e7
constanzauanini <constanza.uanini@admios-sa.com>
2023-01-16 15:33:21 -0300

    Use format instead of toDate

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -72,7 +72,7 @@ export default function DatePicker(props: Props): JSX.Element {
           value={temporalValue}
           onChange={(newValue: any) => {
             setTemporalValue(newValue);
-            props.onChange(newValue?.isValid() ? newValue?.toDate() : null);
+            props.onChange(newValue?.isValid() ? newValue?.format('YYYY-MM-DD') : null);
           }}
           renderInput={renderInput}
           disabled={props.disabled}
