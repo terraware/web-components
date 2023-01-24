4e606b54bd7a4cce28d89bfdd490b7890bbcab75
Charlie Keith <charlie@terraformation.com>
2023-01-24 11:19:50 -0700

    update value_shadow transformer

--- style-dictionary-utils/transforms/value_shadow.js
+++ style-dictionary-utils/transforms/value_shadow.js
@@ -11,8 +11,8 @@ module.exports = {
     const { x, y, blur, spread, color, opacity } = prop.value;
 
     // convert hex code to rgba string
-    const shadowColor = tinycolor(color.value);
-    shadowColor.setAlpha(opacity.value);
+    const shadowColor = tinycolor(color.value ?? color);
+    shadowColor.setAlpha(opacity.value ?? opacity);
     shadowColor.toRgbString();
 
     return `${x.value ?? x} ${y.value ?? y} ${blur.value ?? blur} ${spread.value ?? spread} ${shadowColor}`;
