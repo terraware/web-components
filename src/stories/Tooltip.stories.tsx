aae1ca19a99e2612e8f70d021ea957745b3dfb83
Alex <alex@terraformation.com>
2023-03-03 10:00:32 -0800

    Remove trailing whitespace

--- src/stories/Tooltip.stories.tsx
+++ src/stories/Tooltip.stories.tsx
@@ -9,7 +9,7 @@ export default {
   component: Tooltip,
   argTypes: {
     placement: {
-      options: ['bottom-end', 'bottom-start', 'bottom', 'left-end', 'left-start', 
+      options: ['bottom-end', 'bottom-start', 'bottom', 'left-end', 'left-start',
                 'left', 'right-end', 'right-start', 'right', 'top-end', 'top-start', 'top'],
       control: { type: 'radio' },
     },
