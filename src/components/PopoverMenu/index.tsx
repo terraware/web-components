a50e1fe793df38a5b2c77cf41a63abf0f8918a7c
Karthik Balachandran <karthik@terraformation.com>
2023-03-27 16:09:05 -0700

    Componentize popover menu (user profile menu)
    - copied over from terraware-web and refactored for reusability
    - used in terraware-web and now in admin-portal

---
+++ src/components/PopoverMenu/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import Icon from '../Icon/Icon';
+import { IconButton, Theme } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import Popover, { Section } from './Popover';
+import { DropdownItem } from '../types';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  iconContainer: {
+    height: '48px',
+    borderRadius: '16px',
+    padding: theme.spacing(1.5, 2),
+  },
+  icon: {
+    width: '32px',
+    height: '32px',
+  },
+  chevronDown: {
+    marginLeft: '8px',
+    fill: theme.palette.TwClrIcn,
+  },
+}));
+
+export type PopoverMenuProps = {
+  anchor: React.ReactNode;
+  menuSections: Section[];
+  onClick: (selected: DropdownItem) => void;
+};
+
+export default function PopoverMenu({ anchor, menuSections, onClick }: PopoverMenuProps): JSX.Element {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const onItemClick = (selectedItem: DropdownItem) => {
+    onClick(selectedItem);
+    handleClose();
+  };
+
+  return (
+    <div>
+      <IconButton onClick={handleClick} size='small' className={classes.iconContainer}>
+        {anchor}
+        <Icon name='chevronDown' size='medium' className={classes.chevronDown} />
+      </IconButton>
+      <Popover
+        sections={menuSections}
+        handleClick={onItemClick}
+        anchorElement={anchorEl}
+        setAnchorElement={setAnchorEl}
+      />
+    </div>
+  );
+}
---
+++ src/stories/PopoverMenu.stories.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
+import { Story } from '@storybook/react';
+import PopoverMenu, { PopoverMenuProps } from '../components/PopoverMenu';
+import { DropdownItem } from '../components/types';
+
+export default {
+  title: 'PopoverMenu',
+  component: PopoverMenu,
+};
+
+const PopoverMenuTemplate: Story<PopoverMenuProps> = (args) => {
+  return <PopoverMenu {...args} />;
+};
+
+export const Default = PopoverMenuTemplate.bind({});
+
+Default.args = {
+  anchor: (
+    <>
+      <span>Test</span>
+      <span>Anchor</span>
+    </>
+  ),
+  menuSections: [
+    [{ label: 'Menu Above', value: 'Menu Above' }],
+    [{ label: 'Menu Below', value: 'Menu Below' }],
+  ],
+  onClick: (item: DropdownItem) => window.alert(`you clicked ${item.value}`),
+};
