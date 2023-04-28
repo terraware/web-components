71bb74d5303fef765da416042c8710a34ded597e
karthikbtf <karthik@terraformation.com>
2023-04-28 18:42:07 +0000

    n/a nothing just a npm version bump

--- src/style-dictionary-dist/TerrawareTheme.ts
+++ src/style-dictionary-dist/TerrawareTheme.ts
@@ -1,3 +1,5 @@
+import { PaletteOptions } from '@mui/material/styles/createPalette';
+
 declare module '@mui/material/styles/createPalette' {
   export interface Palette {
     TwClrBaseBlack?: React.CSSProperties['color'];
