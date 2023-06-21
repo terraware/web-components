c89a08f88efe151e5800a311dcaecaa7adf142e9
Karthik B <karthik@terraformation.com>
2023-06-21 15:05:33 -0700

    SW-3712 add a fillColor property on Icon component (#396)
    - make it easier to fill an icon with color
    - avoids having to override fill with nested css
    - did not extend Button with similar pattern, different use-case (icon fill is propagated from button)

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,16 +1,31 @@
+import React from 'react';
+import { makeStyles } from '@mui/styles';
 import { Size } from '../Size';
 import icons, { IconName } from './icons';
 import './styles.scss';
-import React from 'react';
+
+type StyleProps = {
+  fillColor?: string;
+};
+
+const useStyles = makeStyles(() => ({
+  icon: {
+    '& path': {
+      fill: (props: StyleProps) => props.fillColor ?? 'inherit',
+    }
+  }
+}));
 
 export interface Props {
   name: IconName;
   size?: Size;
   className?: string;
+  fillColor?: string;
 }
 
-export default function Icon({ size = 'small', name, className }: Props): JSX.Element {
+export default function Icon({ size = 'small', name, className, fillColor }: Props): JSX.Element {
+  const classes = useStyles({ fillColor });
   const SVGComponent = icons[name];
 
-  return <SVGComponent className={`tw-icon tw-icon--${size} ${className ?? className}`} />;
+  return <SVGComponent className={`tw-icon tw-icon--${size} ${className ?? className} ${classes.icon}`} />;
 }
