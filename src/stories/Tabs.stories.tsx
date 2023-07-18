8f78c3c2db6bceecc5164fbfe173167dbcdedf8d
Karthik Balachandran <karthik@terraformation.com>
2023-07-17 17:00:38 -0700

    Tab component enhancements
    - allow setting the initialized tab
    - callback when tab is changed
    - this is so we can support storing the tab in the url parameter and restore tab based on url parameter

--- src/stories/Tabs.stories.tsx
+++ src/stories/Tabs.stories.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from 'react';
 import { Story } from '@storybook/react';
-import React from 'react';
 import Tabs, { TabsProps } from '../components/Tabs';
 
 export default {
@@ -11,11 +11,33 @@ const Template: Story<TabsProps> = (args) => {
   return <Tabs {...args} />;
 };
 
+const OverrideTemplate: Story<TabsProps> = (args) => {
+  const [activeTab, setActiveTab] = useState<string>('Tab1');
+
+  const onTabChange = (tab: string) => {
+    if (tab !== 'Tab3') {
+      setActiveTab(tab);
+    }
+  };
+
+  return <Tabs {...args} activeTab={activeTab} onTabChange={onTabChange} />;
+};
+
 export const Default = Template.bind({});
 
 Default.args = {
   tabs: [
     { label: 'Tab1', children: 'tab1 contents', disabled: false },
     { label: 'Tab2', children: 'tab2 contents', disabled: false },
+  ],
+};
+
+export const OverrideActiveTab = OverrideTemplate.bind({});
+
+OverrideActiveTab.args = {
+  tabs: [
+    { label: 'Tab1', children: 'tab1 contents', disabled: false },
+    { label: 'Tab2', children: 'tab2 contents', disabled: false },
+    { label: 'Tab3', children: 'tab3 contents', disabled: false },
   ],
 };
