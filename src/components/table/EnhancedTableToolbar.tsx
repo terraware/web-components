c7350841ced9ba32f41c803b016f1c9a6e809ff5
Nick Graziano <nick.graziano@gmail.com>
2023-12-29 13:57:09 -0700

    SW-4503 Add the ability to inject a JSX element into the top bar buttons of the Table

--- src/components/table/EnhancedTableToolbar.tsx
+++ src/components/table/EnhancedTableToolbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Theme, Toolbar, Typography } from '@mui/material';
-import { TopBarButton } from '.';
+import { isTopBarButton, TopBarButton } from '.';
 import Button from '../Button/Button';
 import { makeStyles } from '@mui/styles';
 import Tooltip from '../Tooltip/Tooltip';
@@ -20,7 +20,7 @@ const styles = makeStyles((theme: Theme) => ({
 interface EnhancedTableToolbarProps {
   numSelected: number;
   renderNumSelectedText?: (numSelected: number) => string;
-  topBarButtons?: TopBarButton[];
+  topBarButtons?: (TopBarButton | JSX.Element)[];
 }
 
 export default function EnhancedTableToolbar(props: EnhancedTableToolbarProps): JSX.Element | null {
@@ -32,20 +32,24 @@ export default function EnhancedTableToolbar(props: EnhancedTableToolbarProps): 
       <Typography color='inherit' variant='subtitle1' component='div' className={classes.flexText}>
         {renderNumSelectedText(numSelected)}
       </Typography>
-      {topBarButtons?.map((tbButton, index) => (
-        <Tooltip title={tbButton.tooltipTitle} key={index}>
-          <Button
-            className={classes.buttonSpacing}
-            key={tbButton.buttonText}
-            label={tbButton.buttonText}
-            priority='secondary'
-            type={tbButton.buttonType}
-            onClick={tbButton.onButtonClick}
-            icon={tbButton.icon}
-            disabled={tbButton.disabled}
-          />
-        </Tooltip>
-      ))}
+      {topBarButtons?.map((tbButton: TopBarButton | JSX.Element, index) =>
+        isTopBarButton(tbButton) ? (
+          <Tooltip title={tbButton.tooltipTitle} key={index}>
+            <Button
+              className={classes.buttonSpacing}
+              key={tbButton.buttonText}
+              label={tbButton.buttonText}
+              priority='secondary'
+              type={tbButton.buttonType}
+              onClick={tbButton.onButtonClick}
+              icon={tbButton.icon}
+              disabled={tbButton.disabled}
+            />
+          </Tooltip>
+        ) : (
+          tbButton
+        )
+      )}
     </Toolbar>
   ) : null;
 }
