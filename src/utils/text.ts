b81131fcc623d425a8a1d03530b624f78a3d0e95
Alex <alex@terraformation.com>
2024-02-22 20:59:56 -0800

    lint

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -3,10 +3,10 @@ const isWhitespaces = (str: string) => str.match(/^\s+$/);
 export const titleCase = (str: string) =>
         str.split(' ').map((word) => {
                 if(word !== word.toUpperCase()){
-                        let lowerCaseWord = word.toLowerCase();
+                        const lowerCaseWord = word.toLowerCase();
+
                         return(lowerCaseWord.replace(lowerCaseWord.charAt(0), lowerCaseWord.charAt(0).toUpperCase()));
-                }
-                else{
+                } else{
                         return(word);
                 }
         }).join(' ');
