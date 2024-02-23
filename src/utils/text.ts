318b50d0dd8aea3e54238f66a57a962f470381b3
Alex <alex@terraformation.com>
2024-02-22 20:55:56 -0800

    SW-4918 Don't make acronyms title case

--- src/utils/text.ts
+++ src/utils/text.ts
@@ -1,8 +1,14 @@
 const isWhitespaces = (str: string) => str.match(/^\s+$/);
 
 export const titleCase = (str: string) =>
-        str.toLowerCase().split(' ')
-        .map((word) => word.replace(word.charAt(0), word.charAt(0).toUpperCase()))
-        .join(' ');
+        str.split(' ').map((word) => {
+                if(word !== word.toUpperCase()){
+                        let lowerCaseWord = word.toLowerCase();
+                        return(lowerCaseWord.replace(lowerCaseWord.charAt(0), lowerCaseWord.charAt(0).toUpperCase()));
+                }
+                else{
+                        return(word);
+                }
+        }).join(' ');
 
 export default isWhitespaces;
