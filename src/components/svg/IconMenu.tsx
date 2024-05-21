e4a4c8782383185c48258bbdad918ada7961b0b5
Alex Edwards <104874529+alexTerraformation@users.noreply.github.com>
2024-05-21 09:33:44 -0700

    SW-5384 - Add library book icon and export (#553)
    * SW-5384
    
    Add new library icon

--- src/components/svg/IconMenu.tsx
+++ src/components/svg/IconMenu.tsx
@@ -7,7 +7,7 @@ interface SVGRProps {
 const SvgIconMenu = ({ title, titleId, ...props }: SVGProps<SVGSVGElement> & SVGRProps) => (
   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' aria-labelledby={titleId} {...props}>
     {title ? <title id={titleId}>{title}</title> : null}
-    <path d='M2.75 4.5a.75.75 0 1 0 0 1.5h18.5a.75.75 0 1 0 0-1.5H2.75Zm0 6.75a.751.751 0 1 0 0 1.5h18.5a.75.75 0 1 0 0-1.5H2.75Zm0 6.75a.751.751 0 1 0 0 1.5h18.5a.75.75 0 1 0 0-1.5H2.75Z' />
+    <path d='M2.75 4.5a.75.75 0 1 0 0 1.5h18.5a.75.75 0 1 0 0-1.5zm0 6.75a.751.751 0 1 0 0 1.5h18.5a.75.75 0 1 0 0-1.5zm0 6.75a.751.751 0 1 0 0 1.5h18.5a.75.75 0 1 0 0-1.5z' />
   </svg>
 );
 export default SvgIconMenu;
--- src/components/svg/MenuVertical.tsx
+++ src/components/svg/MenuVertical.tsx
@@ -7,7 +7,7 @@ interface SVGRProps {
 const SvgMenuVertical = ({ title, titleId, ...props }: SVGProps<SVGSVGElement> & SVGRProps) => (
   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' aria-labelledby={titleId} {...props}>
     {title ? <title id={titleId}>{title}</title> : null}
-    <path d='M12 16.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Zm0-6a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Zm0-6a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3Z' />
+    <path d='M12 16.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m0-6a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3m0-6a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3' />
   </svg>
 );
 export default SvgMenuVertical;
