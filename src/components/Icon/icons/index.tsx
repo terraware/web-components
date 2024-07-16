69737343077e918275893fa166f7c26ac73b852b
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-07-16 16:10:27 -0700

    Added success-filled icon

--- src/components/Icon/icons/index.tsx
+++ src/components/Icon/icons/index.tsx
@@ -99,6 +99,7 @@ import Species from '../../svg/Species';
 import Species2 from '../../svg/Species2';
 import Spinner from '../../svg/Spinner';
 import Success from '../../svg/Success';
+import SuccessFilled from '../../svg/SuccessFilled';
 import Touchscreen from '../../svg/Touchscreen';
 import UploadCloud from '../../svg/UploadCloud';
 import Warning from '../../svg/Warning';
@@ -206,6 +207,7 @@ export type IconName =
   | 'species2'
   | 'spinner'
   | 'success'
+  | 'successFilled'
   | 'touchscreen'
   | 'uploadCloud'
   | 'warning'
@@ -317,6 +319,7 @@ const icons: Record<IconName, SVGComponent> = {
   species2: Species2,
   spinner: Spinner,
   success: Success,
+  successFilled: SuccessFilled,
   touchscreen: Touchscreen,
   uploadCloud: UploadCloud,
   warning: Warning,
