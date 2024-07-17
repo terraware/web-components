862b55ef63eb9d4e32de5588c1c1335058db24ae
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-07-16 17:02:04 -0700

    Added success-filled icon (#618)
    ![Screenshot 2024-07-16 at 4.10.09 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/NOxkRPuhAfT4F7nmdXtG/66da4997-2786-423d-8445-3a8fc06282d4.png)

---
+++ src/components/svg/SuccessFilled.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+interface SVGRProps {
+  title?: string;
+  titleId?: string;
+}
+const SvgSuccessFilled = ({ title, titleId, ...props }: SVGProps<SVGSVGElement> & SVGRProps) => (
+  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' aria-labelledby={titleId} {...props}>
+    {title ? <title id={titleId}>{title}</title> : null}
+    <path d='M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2m4.28 8.28-5 5a.75.75 0 0 1-1.06 0l-2.5-2.5a.75.75 0 1 1 1.06-1.06l1.97 1.97 4.469-4.47a.75.75 0 0 1 1.062 1.06' />
+  </svg>
+);
+export default SvgSuccessFilled;
