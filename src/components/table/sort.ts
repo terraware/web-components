eb6a0bf4a1c55822952bac0d80676b432ebbb353
constanzauanini <constanza.uanini@admios-sa.com>
2024-08-01 17:51:04 -0300

    Fix number order condition

--- src/components/table/sort.ts
+++ src/components/table/sort.ts
@@ -2,7 +2,7 @@ export function descendingComparator<T>(a: T, b: T, orderBy: keyof T, order: Sor
   // first attempt to parse into a numeric value and compare
   const numCompare = descendingNumComparator(a, b, orderBy);
   if (numCompare !== null) {
-    return order === 'desc' ? numCompare : numCompare * -1;
+    return order === 'desc' ? numCompare * -1 : numCompare;
   }
 
   // if non-numeric, compare using the javascript built-in compare for this type
