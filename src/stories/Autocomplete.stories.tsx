39ecaccbc9ef37de0341cb18cbaef41a5605bee7
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-09-17 12:35:10 -0700

    Upgrades AutoComplete to better support async fetching of results

--- src/stories/Autocomplete.stories.tsx
+++ src/stories/Autocomplete.stories.tsx
@@ -1,7 +1,7 @@
 import { action } from '@storybook/addon-actions';
 import { Story } from '@storybook/react';
 import { Box, useTheme } from '@mui/material';
-import React from 'react';
+import React, { useCallback } from 'react';
 import Autocomplete, { ValueType, Props as AutocompleteProps } from '../components/Autocomplete/Autocomplete';
 
 export default {
@@ -32,9 +32,70 @@ const Template: Story<AutocompleteProps> = (args) => {
   );
 };
 
+const AsyncTemplate: Story<AutocompleteProps> = (args) => {
+  const theme = useTheme();
+
+  const [open, setOpen] = React.useState<boolean>(false);
+  const [loading, setLoading] = React.useState<boolean>(false);
+  const [selected, setSelected] = React.useState<ValueType>();
+  const [options, setOptions] = React.useState<ValueType[]>([]);
+
+  const sleep = (duration: number): Promise<void> => {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        resolve();
+      }, duration);
+    });
+  };
+
+  const load = useCallback(async () => {
+    setLoading(true);
+    await sleep(1e3);
+    setLoading(false);
+    setOptions([...args.options]);
+  }, [setLoading, setOptions]);
+
+  const handleOpen = useCallback(async () => {
+    setOpen(true);
+    await load();
+  }, [setOpen, load]);
+
+  const handleClose = () => {
+    setOpen(false);
+    setOptions([]);
+  };
+
+  const handleChange = (value: ValueType) => {
+    action('onChange')(value);
+    setSelected(value);
+  };
+
+  return (
+    <Box sx={{ marginTop: '30px' }}>
+      <Autocomplete
+        {...args}
+        open={open}
+        onOpen={() => void handleOpen()}
+        onClose={handleClose}
+        selected={selected}
+        loading={loading}
+        options={options}
+        onChange={handleChange}
+        sx={{
+          backgroundColor: theme.palette.TwClrBaseWhite,
+          width: '300px',
+        }}
+      />
+    </Box>
+  );
+};
+
 export const Default = Template.bind({});
 
 export const Complex = Template.bind({});
+
+export const AsyncDefault = AsyncTemplate.bind({});
+
 
 Default.args = {
   id: '1',
@@ -74,4 +135,12 @@ Complex.args = {
   errorText: '',
   isEqual: (option: any, value: any) => option.value === value.value,
   tooltipTitle: 'Hello world!',
+};
+
+AsyncDefault.args = {
+  id: '3',
+  label: 'Test',
+  options: ['Test 1', 'Test 2', 'Hello'],
+  freeSolo: true,
+  errorText: '',
 };
