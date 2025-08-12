d13703fbac9dabbba2ae78bd2377a640ce76dd3e
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2025-08-12 14:13:06 -0700

    Packaged into a map component

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+  input: 'src/index.ts', // your library entry
+  output: [
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'named',
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve({ extensions: ['.js', '.ts', '.tsx'] }),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json', sourceMap: true }),
+    postcss(),
+  ],
+};
