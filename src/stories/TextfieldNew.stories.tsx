a356db27ab5b57892a4fcb0f653cbdbb5bd558e2
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2025-08-29 19:34:57 -0700

    Added markdown support for TextField (#691)
    [Screen Recording 2025-08-29 at 4.04.31 PM.mov <span class="graphite__hidden">(uploaded via Graphite)</span> <img class="graphite__hidden" src="https://app.graphite.dev/user-attachments/thumbnails/d1dbef6a-f1b6-4f31-b30e-6c07c6dff994.mov" />](https://app.graphite.dev/user-attachments/video/d1dbef6a-f1b6-4f31-b30e-6c07c6dff994.mov)

--- src/stories/TextfieldNew.stories.tsx
+++ src/stories/TextfieldNew.stories.tsx
@@ -28,7 +28,7 @@ export default {
 };
 
 const Template: Story<TextFieldProps> = (args) => {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState(args.value ?? 0);
   const handleChange = (v: unknown) => {
     setValue(v as string);
   };
@@ -59,6 +59,8 @@ export const WithTooltip = Template.bind({});
 
 export const TextArea = Template.bind({});
 
+export const TextAreaWithMarkdown = Template.bind({});
+
 export const TextAreaWithTruncate = Template.bind({});
 
 export const NumberField = NumberTemplate.bind({});
@@ -72,6 +74,7 @@ Default.args = {
   warningText: '',
   readonly: false,
   display: false,
+  markdown: false,
   type: 'text',
   autoFocus: false,
   preserveNewlines: false,
@@ -86,6 +89,7 @@ WithTooltip.args = {
   warningText: '',
   readonly: false,
   display: false,
+  markdown: false,
   type: 'text',
   tooltipTitle: 'Hello world!',
   autoFocus: false,
@@ -101,9 +105,140 @@ TextArea.args = {
   warningText: '',
   readonly: false,
   display: false,
+  markdown: false,
   type: 'textarea',
   autoFocus: false,
   preserveNewlines: true,
+};
+
+TextAreaWithMarkdown.args = {
+  label: 'Field Label',
+  disabled: false,
+  helperText: 'Help text.',
+  placeholder: 'Placeholder...',
+  errorText: '',
+  warningText: '',
+  readonly: false,
+  display: true,
+  markdown: true,
+  type: 'textarea',
+  autoFocus: false,
+  preserveNewlines: true,
+  value: `\
+# h1 Heading
+## h2 Heading
+### h3 Heading
+#### h4 Heading
+##### h5 Heading
+###### h6 Heading
+
+
+## Horizontal Rules
+
+___
+
+---
+
+***
+
+
+## Typographic replacements
+
+"Double quotes"
+
+'single quotes'
+
+
+## Emphasis
+
+**This is bold text**
+
+__This is bold text__
+
+*This is italic text*
+
+_This is italic text_
+
+~~Strikethrough~~
+
+
+## Blockquotes
+
+
+> Blockquotes can also be nested...
+>> ...by using additional greater-than signs right next to each other...
+> > > ...or with spaces between arrows.
+
+
+## Lists
+
+Unordered
+
++ Create a list by starting a line with \`+\`, \`-\`, or \`*\`
++ Sub-lists are made by indenting 2 spaces:
+  - Marker character change forces new list start:
+    * Ac tristique libero volutpat at
+    + Facilisis in pretium nisl aliquet
+    - Nulla volutpat aliquam velit
++ Very easy!
+
+Ordered
+
+1. Lorem ipsum dolor sit amet
+2. Consectetur adipiscing elit
+3. Integer molestie lorem at massa
+
+
+1. You can use sequential numbers...
+1. ...or keep all the numbers as \`1.\`
+
+Start numbering with offset:
+
+57. foo
+1. bar
+
+
+## Code
+
+Inline \`code\`
+
+Indented code
+
+    // Some comments
+    line 1 of code
+    line 2 of code
+    line 3 of code
+
+
+Block code "fences"
+
+\`\`\`
+Sample text here...
+\`\`\`
+
+## Tables
+
+| Option | Description |
+| ------ | ----------- |
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+
+Right aligned columns
+
+| Option | Description |
+| ------:| -----------:|
+| data   | path to data files to supply the data that will be passed into templates. |
+| engine | engine to be used for processing templates. Handlebars is the default. |
+| ext    | extension to be used for dest files. |
+
+
+## Links
+
+[link text](http://terraware.io)
+
+Autoconverted link http://terraware.io
+`,
 };
 
 TextAreaWithTruncate.args = {
@@ -115,6 +250,7 @@ TextAreaWithTruncate.args = {
   warningText: '',
   readonly: false,
   display: false,
+  markdown: false,
   type: 'textarea',
   autoFocus: false,
   preserveNewlines: true,
@@ -138,6 +274,7 @@ NumberField.args = {
   warningText: '',
   readonly: false,
   display: false,
+  markdown: false,
   min: 5,
   max: 25,
   autoFocus: false,
