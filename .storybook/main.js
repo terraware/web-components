6491bea11c5a51c55538af63aacbffc7c8ed6f7e
Isaac Hudson <isaac.hudson@terraformation.com>
2026-02-12 14:52:39 -0800

    Add prettier to CI and fix formatting issues (#734)
    A [renovate PR](https://github.com/terraware/web-components/pull/727) updates a prettier plugin, and testing this showed that prettier wasn't running in CI. Add formatting check to CI and fix the existing issues.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,6 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-create-react-app',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-create-react-app'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
@@ -15,9 +11,7 @@ module.exports = {
   },
   webpackFinal: async (config) => {
     // Disable ESLint in Storybook build to avoid build failures
-    config.plugins = config.plugins.filter(
-      plugin => plugin.constructor.name !== 'ESLintWebpackPlugin'
-    );
+    config.plugins = config.plugins.filter((plugin) => plugin.constructor.name !== 'ESLintWebpackPlugin');
 
     return config;
   },
